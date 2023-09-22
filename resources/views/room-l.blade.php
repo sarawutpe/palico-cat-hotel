2995ca90392d994c9778e1c00006cf5754b7c10d
Sarawut Pe <mr.sarawutpe@gmail.com>
2023-09-23 00:44:12 +0700

    room img

--- resources/views/room-l.blade.php
+++ resources/views/room-l.blade.php
@@ -3,5 +3,11 @@
 @section('content')
     <div class="container-lg">
         <h2>ห้องพักขนาด l</h2>
+
+        <div class="d-flex flex-wrap gap-4">
+            <img src="{{ asset('assets/img/l1.jpg') }}" width="500px" alt="">
+            <img src="{{ asset('assets/img/l2.jpg') }}" width="500px" alt="">
+            <img src="{{ asset('assets/img/l3.jpg') }}" width="500px" alt="">
+        </div>
     </div>
 @endsection
